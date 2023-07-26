3c120e95700afea2b4a53d62ef8d7b5413881776
Susie Gordon <susieyoungchoi@gmail.com>
2023-07-25 21:08:34 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Book = (props) => {
+  return (
+    <>
+      <h3>{props.title}</h3>
+      <p>{props.author}</p>
+    </>
+  )
+}
+
+
+// method 2:
+// const GrandChildOne = (props) => {
+//   return (
+//     <>
+//       <h1>{props.bookTitle}</h1>
+//       <h1>{props.author}</h1>
+//     </>
+//   )
+// }
+
+
+// const ChildOne = (props) => {
+//   return (
+//     <h1>{props.bookTitle}</h1>
+//   )
+// }
+
+// const ChildOneVersionTwo = (props) => {
+//   return (
+//     <>
+//       <h1>{props.bookTitle}</h1>
+//       <GrandChildOne
+//         bookTitle={props.bookTitle}
+//         author={props.author}
+//       />
+//     </>
+//   )
+// }
+
+
+// method 3:
+// const GrandChildOne = (props) => {
+//   return (
+//     <>
+//       <h3>{props.bookTitle}</h3>
+//       <h3>{props.author}</h3>
+//     </>
+//   )
+// }
+
+
+// const ChildOne = (props) => {
+//   return (
+//     <>
+//       <h1>{props.bookTitle}</h1>
+//       {props.author && <GrandChildOne bookTitle={props.bookTitle} author={props.author}/>}
+//     </>
+//   )
+// }
+
+
+// method 4:
+// const Books = (props) => {
+//   return (
+//     <>
+//       <h1>{props.bookTitle}</h1>
+//       {props.author && <BooksWithAuthor bookTitle={props.bookTitle} author={props.author}/>}
+//     {/* either or work */}
+//       {/* {props.author ? <BooksWithAuthor bookTitle={props.bookTitle} author={props.author}/> : null} */}
+//     </>
+//   )
+// }
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Books</h1>
+
+     <Book
+        title="Harry Potter"
+     />
+
+      <Book
+        title="Lord of the Rings"
+     />
+
+      <Book
+        title="Game of Thrones"
+        author="George R. R. Martin"
+     />
+
     </div>
   );
 }
